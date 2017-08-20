d6614bc4af7491e97b007983b74a1a7123a6510e
0dysseas <kaziolas.od@gmail.com>
2017-08-20 17:37:10 +0300

    Added functionality to retrieve all the news

--- get_news.py
+++ get_news.py
@@ -1,14 +1,19 @@
 import requests
+import logging
 from get_news_helpers import print_json_object
 
 # TODO-me:Refactor below in a class
 
 # TODO-me: Add an attribution link that reads "Powered by NewsAPI"
 
-latest_news = {}
-API_KEY = 'XXX-XXX-XXX'
+# TODO-me: Put the list of links in another module(base_API_endpoints) and import it from there using Queues
 
-# TODO-me: Put the list of links in another module and import it from there
+
+logging.basicConfig(level=logging.INFO)
+
+# API_KEY = 'XXX-XXX-XXX'
+
+# Base API endpoints
 guardian = 'https://newsapi.org/v1/articles?source=the-guardian-uk&sortBy=latest&apiKey=' + API_KEY
 al_jaz = 'https://newsapi.org/v1/articles?source=al-jazeera-english&sortBy=latest&apiKey=' + API_KEY
 hacker_news_top = 'https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=' + API_KEY
@@ -19,20 +24,37 @@ ny_times = 'https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top
 
 news_sources = [guardian, al_jaz, hacker_news_top, nation_geog_top, tech_radar_top, next_web, ny_times]
 
-# for key, value in enumerate(news_sources):
-#     print key, value
+latest_news = list()
 
-# Latest Guardian headlines
-response = requests.get(
-    guardian).json()
 
-print_json_object(response)
+def form_news_structure(json_news):
+    for k, v in enumerate(json_news['articles']):
+        # print {response['source']: {v['title']: v['url']}}
+        latest_news.append({json_news['source']: {v['title']: v['url']}})
+        # print key, value
+
+
+def retrieve_news(sources):
+    for key, value in enumerate(sources):
+        response = requests.get(value).json()
+        print_json_object(response)
+        form_news_structure(response)
+
+
+if __name__ == '__main__':
+
+    logging.info('Retrieving news...')
+
+    retrieve_news(news_sources)  # TODO-me: Enclose in try-catch blocks?
+
+    # for k, v in enumerate(latest_news):
+    #     print k, v
 
 # Extract {source:{title:url}}, except for National_geo where extract {source:{title:urlToImage}}
 # print {response['source']: {response['title']: response['url']}}
-for k, v in enumerate(response['articles']):
-    print {response['source']: {v['title']: v['url']}}
-
+# for k, v in enumerate(response['articles']):
+#     print {response['source']: {v['title']: v['url']}}
+#
 
 
 
