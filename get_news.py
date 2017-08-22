bac29445a889366d88cc42d8b860d9dcce0510fd
Odysseus Kaziolas <kaziolas.od@gmail.com>
2017-08-22 22:11:08 +0300

    Implemented the datastructure to hold thenews sources

--- get_news.py
+++ get_news.py
@@ -2,16 +2,18 @@ import requests
 import logging
 from get_news_helpers import print_json_object
 
-# TODO-me:Refactor below in a class
-
 # TODO-me: Add an attribution link that reads "Powered by NewsAPI"
 
-# TODO-me: Put the list of links in another module(base_API_endpoints) and import it from there using Queues
+# TODO-me: Put the list of links in another module(base_API_endpoints) and import it from there using Queues to operate
+# on it
+
+# TODO-me: Split news and sports sources?
 
 
 logging.basicConfig(level=logging.INFO)
 
-# API_KEY = 'XXX-XXX-XXX'
+API_KEY = 'XXX-XXX-XXX'
+
 
 # Base API endpoints
 guardian = 'https://newsapi.org/v1/articles?source=the-guardian-uk&sortBy=latest&apiKey=' + API_KEY
@@ -24,37 +26,52 @@ ny_times = 'https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top
 
 news_sources = [guardian, al_jaz, hacker_news_top, nation_geog_top, tech_radar_top, next_web, ny_times]
 
-latest_news = list()
+latest_news = dict()
 
 
-def form_news_structure(json_news):
-    for k, v in enumerate(json_news['articles']):
-        # print {response['source']: {v['title']: v['url']}}
-        latest_news.append({json_news['source']: {v['title']: v['url']}})
-        # print key, value
+class News(object):
+
+    def __init__(self, sources):
+        self.sources = sources
+
+    def __repr__(self):
+        return self.sources
+
+    @staticmethod
+    def delete_redundant_items(json_news, keys_to_del):
+        for item in keys_to_del:
+            del json_news[item]
+
+        return json_news
+
+    def form_news_structure(self, json_news):
+        keys_to_remove = ['status', 'sortBy']
+        sub_keys_to_remove = ['description', 'author', 'publishedAt']  # TODO-me: Handle the National Geographic case
+
+        self.delete_redundant_items(json_news, keys_to_remove)
+
+        for _, article in enumerate(json_news['articles']):
+            self.delete_redundant_items(article, sub_keys_to_remove)
+
+    def retrieve_news(self):
+        for key, value in enumerate(self.sources):
+            response = requests.get(value).json()
+            # print_json_object(response)
+            self.form_news_structure(response)
+            print_json_object(response)
 
 
-def retrieve_news(sources):
-    for key, value in enumerate(sources):
-        response = requests.get(value).json()
-        print_json_object(response)
-        form_news_structure(response)
+def main():
+    news = News(news_sources)
+    logging.info('Retrieving news...')
+    news.retrieve_news()
 
 
 if __name__ == '__main__':
 
-    logging.info('Retrieving news...')
+    main()
 
-    retrieve_news(news_sources)  # TODO-me: Enclose in try-catch blocks?
 
-    # for k, v in enumerate(latest_news):
-    #     print k, v
-
-# Extract {source:{title:url}}, except for National_geo where extract {source:{title:urlToImage}}
-# print {response['source']: {response['title']: response['url']}}
-# for k, v in enumerate(response['articles']):
-#     print {response['source']: {v['title']: v['url']}}
-#
 
 
 
