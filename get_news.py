11396e2350d0f585dd93a7aaa2ed74f62bcd54cf
0dysseas <kaziolas.od@gmail.com>
2017-10-03 08:29:57 +0300

    Implemented periodic scheduling

--- get_news.py
+++ get_news.py
@@ -1,12 +1,22 @@
 import logging
 import os
+import signal
 import sys
 from time import time
 from Queue import Queue
 from threading import Thread
 
+import gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+
+from gi.repository import Gtk, AppIndicator3, GObject
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR, EVENT_ALL
 import requests
 
+from news_indicator import NewsIndicator
 from news_helpers import print_json_object, get_news_sources_from_file, delete_redundant_items
 
 
@@ -20,19 +30,25 @@ NUM_THREADS = 8
 
 logging.basicConfig(level=logging.INFO)
 
-try:
+try:  # TODO-me: Refactor the two try clauses
     API_KEY = str(os.environ.get('NEWS_API_KEY'))
 except KeyError:
     print ('Please save your personal NewsAPI key in a "NEWS_API_KEY" env variable.')
     sys.exit(1)
 
+try:
+    sched = BlockingScheduler()
+except ImportError:
+    print ('Failed to import Scheduler')
+    sys.exit(1)
+
 
 class DownloadWorker(Thread):
 
-    def __init__(self, input_queue, out_list):
-        Thread.__init__(self, target=self.download_content)
+    def __init__(self, input_queue, out_queue):
+        Thread.__init__(self, target=self.download_content)  # TODO-me: Add intervals for continuous thread running i.e. downloading of sources
         self.input_queue = input_queue
-        self.out_list = out_list
+        self.out_queue = out_queue
 
     def __repr__(self):
         return self.input_queue
@@ -44,13 +60,14 @@ class DownloadWorker(Thread):
         # TODO-me: Optimize below code!
         json_1 = delete_redundant_items(json_news, keys_to_remove)
 
-        for _, article in enumerate(json_1['articles']):
+        # Get the first four articles from each source
+        for _, article in enumerate(json_1['articles'][:2]):
             json_2 = delete_redundant_items(article, sub_keys_to_remove)
 
-            self.out_list.append(json_2)
+            self.out_queue.put(json_2)
 
-        for k, v in enumerate(self.out_list):
-            print k, v
+        # for k, v in enumerate(self.out_queue):
+        #     print k, v
         return json_news
 
     def download_content(self):
@@ -62,13 +79,11 @@ class DownloadWorker(Thread):
 
             self.input_queue.task_done()
 
-        return None
-
 
 class DownloadNewsWorker(object):
 
-    def __init__(self, output_list):
-        self.output_list = output_list
+    def __init__(self, output_queue):
+        self.output_queue = output_queue
 
     def __repr__(self):
         return self.output_queue
@@ -79,7 +94,7 @@ class DownloadNewsWorker(object):
 
         # Create the worker threads. The number is arbitrary and will be optimized based on performance
         for i in range(NUM_THREADS):
-            download_worker = DownloadWorker(input_queue, self.output_list)
+            download_worker = DownloadWorker(input_queue, self.output_queue)
             download_worker.setDaemon(True)  # Daemonize the working thread so as the main thread always exits
             download_worker.start()
 
@@ -92,17 +107,51 @@ class DownloadNewsWorker(object):
         input_queue.join()
 
 
+@sched.scheduled_job('interval', minutes=2, name='my_job_1')  # TODO-me: Change the job_id
 def main():
 
-    start = time()
+    # start = time()
 
-    test_list = list()
-    logging.info('Retrieving news...')
-    download = DownloadNewsWorker(test_list)
+    output_queue = Queue()
+
+    out_list = list()
+
+    print('here')
+    # logging.info('Retrieving news...')
+    download = DownloadNewsWorker(output_queue)
     download.retrieve_news()
-    print("Finished in: {}".format(time()-start))
+    # logging.info('Printing output queue...')
+    # while not output_queue.empty():
+    #     print (output_queue.get())
+    # logging.info('Output..')
+    while not output_queue.empty():
+        item = output_queue.get()  # Using output queue and then feed that into a list??? That's dummmy.FInd a better way..
+        out_list.append(item)
+
+    print('after')
+    # NewsIndicator(out_list)  # TODO-me: Update the idle thread not the main one
+    # signal.signal(signal.SIGINT, signal.SIG_DFL)
+    # Gtk.main()
+    return out_list
+    # print("Finished in: {}".format(time()-start))
+
+
+# Listener that is triggered when each job is executed
+def my_listener(event):  # TODO-me:Rename this
+    if event.retval:
+        print event.retval
+        NewsIndicator(event.retval)  # Feed the out_list of main() to NewsIndicator
+        Gtk.main()
 
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    sched.add_listener(my_listener, EVENT_JOB_EXECUTED)
+    sched.start()
 
-    main()
+    # print('before')
+    # final_list = main()
+    # print('after')
+    # NewsIndicator(final_list)  #TODO-me: Update the idle thread not the main one
+    # signal.signal(signal.SIGINT, signal.SIG_DFL)
+    # Gtk.main()
--- news_indicator.py
+++ news_indicator.py
@@ -10,22 +10,36 @@ from gi.repository import Gtk, AppIndicator3, GObject
 
 
 class NewsIndicator(object):
-    def __init__(self):
+    def __init__(self, news_list):
         self.app = 'News-Indicator'
+        self.news_list = news_list
+
         absolute_path = os.path.dirname(os.path.abspath(__file__))
         icon = os.path.join(absolute_path, 'assets/news_icon.png')
+
         self.indicator = AppIndicator3.Indicator.new(self.app, icon, AppIndicator3.IndicatorCategory.OTHER)
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-        self.indicator.set_menu(self.create_menu())
+        print('hello')
+        # self.x = self.indicator.get_menu()  #TODO-me:Use this!
+        print('hello_2')
+        self.indicator.set_menu(self.create_menu(self.news_list))
+        print('hello_3')
         self.indicator.set_label('News', self.app)
 
-    def create_menu(self):
-        menu = Gtk.Menu()
-        first_item = Gtk.MenuItem('Test_News_Item')
-        menu.append(first_item)
+    def create_menu(self, menu_items_list):
 
-        separator = Gtk.SeparatorMenuItem()
-        menu.append(separator)
+        # if current_menu and current_menu.get_children():  #TODO-me: Make it work
+        #     print('in here')
+        #     for item in current_menu.get_children():
+        #         current_menu.remove(item)
+
+        menu = Gtk.Menu()
+
+        for k, v in enumerate(menu_items_list):
+            first_item = Gtk.MenuItem(v['title'])
+            menu.append(first_item)
+            separator = Gtk.SeparatorMenuItem()
+            menu.append(separator)
 
         quit_item = Gtk.MenuItem('Quit')
         quit_item.connect('activate', self.stop)
@@ -37,8 +51,9 @@ class NewsIndicator(object):
     def stop(self, source):
         Gtk.main_quit()
 
+#
+# if __name__ == '__main__':
+#     # NewsIndicator()
+#     # signal.signal(signal.SIGINT, signal.SIG_DFL)
+#     # Gtk.main()
 
-if __name__ == '__main__':
-    NewsIndicator()
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    Gtk.main()
