6a9f74f8689078e7de11ce6798507f8f7534eac2
0dysseas <kaziolas.od@gmail.com>
2017-12-19 21:49:50 +0200

    Added trove classifiers

--- setup.py
+++ setup.py
@@ -19,6 +19,19 @@ setup(name=NAME,
       license='GNU Lesser General Public License v3.0',
       packages=['newsindicator'],
       install_requires=REQUIRED,
+      classifiers=[
+        'License :: OSI Approved :: GLPL v3.0 License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+      ],
       data_files=[
           ('/usr/share/applications', ['newsindicator.desktop']),
           ('/usr/share/icons', ['newsindicator_icon.png'])
