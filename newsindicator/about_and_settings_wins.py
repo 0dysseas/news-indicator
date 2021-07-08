09d2d02e92230aea14ace1f74b810befae9af690
0dysseas <kaziolas.od@gmail.com>
2021-07-08 19:32:57 +0300

    chore(window): Change constructor

--- newsindicator/about_and_settings_wins.py
+++ newsindicator/about_and_settings_wins.py
@@ -18,7 +18,7 @@ class AboutWindow(Gtk.Dialog):
     """
 
     def __init__(self):
-        # Init about window
+        # About window constructor
         super(AboutWindow, self).__init__()
         about = Gtk.AboutDialog()
         about.set_program_name('News Indicator')
